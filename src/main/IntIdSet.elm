63ad7d7cfb1041c0657603ba98ba4d2bc85a992f
Nick ten Veen <nveen0930@gmail.com>
2016-06-06 19:04:02 +0200

    added docs to IntIdSet

--- src/main/IntIdSet.elm
+++ src/main/IntIdSet.elm
@@ -1,5 +1,12 @@
 module IntIdSet exposing (..)
 
+{-| Specilization functions for IdSets containing integers as their id.
+
+@docs IntIdSet
+@docs intGenerator
+@docs empty
+-}
+
 import IdSet exposing (..)
 
 {-| IdGenerator for intgers.
@@ -11,5 +18,7 @@ type alias IntIdSet item = IdSet Int Int item
 intGenerator : IdGenerator Int Int
 intGenerator i = (i, i + 1)
 
+{-| Creates an empty set with intGenerator as its IdGenerator and initial seed 0
+-}
 empty: IntIdSet item
 empty = IdSet.empty intGenerator 0
